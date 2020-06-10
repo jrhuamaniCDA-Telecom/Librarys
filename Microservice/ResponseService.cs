1a8e8092e4de4ccf1d3e3d7d566e13d3954ad9a4
Junior <neo.junior.16@gmail.com>
2020-06-10 14:59:36 -0300

    Update files

---
+++ Microservice/ResponseService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservice
+{
+  public class ResponseService<T> where T : class
+  {
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+    public T ResponseData { get; set; }
+  }
+}
